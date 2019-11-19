e8472066692a3f9b5a41ac6b2c2405c7f8cc7914
ttnny <ttnny@users.noreply.github.com>
2019-11-18 16:34:23 -0800

    Update

--- draft/src/Collections.java
+++ draft/src/Collections.java
@@ -1,10 +1,12 @@
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
+import java.lang.reflect.Array;
+import java.util.*;
 
 public class Collections {
-    ArrayList<String> stringList = new ArrayList<>();
-    List<Integer> integerList = new LinkedList<>();
+    List<String> arrayList = new ArrayList<>();
+    List<Integer> linkedList = new LinkedList<>();
+    List<Double> stack = new Stack<>();
+    List<Character> queue = new Queue<>();
+
 
     public static void main(String[] args) {
 

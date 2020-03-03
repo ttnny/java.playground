6ccb764be24df5e13f7a9c69169241edd8a8c18e
ttnny <ttnny@users.noreply.github.com>
2020-03-02 23:33:54 -0800

    Update

--- draft/src/PrimitiveTypes.java
+++ draft/src/PrimitiveTypes.java
@@ -1,7 +1,7 @@
 public class PrimitiveTypes {
     public static void main(String[] args) {
-        int minInt = Integer.MIN_VALUE;
         int maxInt = Integer.MAX_VALUE;
+        int minInt = Integer.MIN_VALUE;
 
         System.out.println(maxInt); //  2147483647
         maxInt++;

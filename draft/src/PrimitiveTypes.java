3823e721d3adada94d17b2a0bbecd66009575b80
ttnny <ttnny@users.noreply.github.com>
2020-03-02 23:24:50 -0800

    Update

--- draft/src/PrimitiveTypes.java
+++ draft/src/PrimitiveTypes.java
@@ -3,10 +3,12 @@ public class PrimitiveTypes {
         int minInt = Integer.MIN_VALUE;
         int maxInt = Integer.MAX_VALUE;
 
-        System.out.println(maxInt);     //  2147483647
-        System.out.println(maxInt + 1); // -2147483648 (Overflow)
+        System.out.println(maxInt); //  2147483647
+        maxInt++;
+        System.out.println(maxInt); // -2147483648 (Overflow)
 
-        System.out.println(minInt);     // -2147483648
-        System.out.println(minInt - 1); //  2147483647 (Underflow)
+        System.out.println(minInt); // -2147483648
+        minInt--;
+        System.out.println(minInt); //  2147483647 (Underflow)
     }
 }

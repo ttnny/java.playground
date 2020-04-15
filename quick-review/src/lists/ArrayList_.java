af0c4dffe1cfe0bf51ebd86458ff668fd135f816
ttnny <ttnny@users.noreply.github.com>
2020-04-14 23:58:44 -0700

    Update

---
+++ quick-review/src/lists/ArrayList_.java
@@ -0,0 +1,13 @@
+package lists;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayList_ {
+    public static void main(String[] args) {
+        // Declare
+        List<Integer> list = new ArrayList<>();
+
+        // Similar to LinkedList
+    }
+}
---
+++ quick-review/src/lists/Stack_.java
@@ -0,0 +1,14 @@
+package lists;
+
+import java.util.Stack;
+
+public class Stack_ {
+    public static void main(String[] args) {
+        // Declare
+        Stack<Integer> stack = new Stack<>();
+
+        // Use
+        stack.push(5);
+        stack.pop();
+    }
+}

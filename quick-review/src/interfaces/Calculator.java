a500b53be0af540938aa934d7fc623c32842c8b4
ttnny <>
2021-03-04 23:26:53 -0800

    Update

---
+++ quick-review/src/interfaces/Calculator.java
@@ -0,0 +1,14 @@
+package interfaces;
+
+public class Calculator {
+    // Using an Interface as a Type: When you define a new interface,
+    // you are defining a new reference data type. You can use
+    // interface names anywhere you can use any other data type name.
+    // If you define a reference variable whose type is an interface,
+    // any object you assign to it must be an instance of a class that implements the interface.
+    CalculatorService service;
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ quick-review/src/interfaces/CalculatorService.java
@@ -0,0 +1,5 @@
+package interfaces;
+
+public interface CalculatorService {
+    public int add(int i, int j);
+}
